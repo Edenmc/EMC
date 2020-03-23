b1247a4354c8b0a32b71b676eb2507b96c6da203
Eden Coimbra <edencoimbra@gmail.com>
2020-03-23 11:01:05 -0700

    First commit

---
+++ projetoQuarentena/src/main/java/br/com/automacao/projetoQuarentena/AbreNavegador.java
@@ -0,0 +1,43 @@
+package br.com.automacao.projetoQuarentena;
+
+import java.awt.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class AbreNavegador extends BaseTest{
+	
+	@Test
+	public void deveAbrirNavegador() {
+		String url = driver.getCurrentUrl();
+		System.out.println("Estams na página;"+url);
+		String titulo = driver .getTitle();
+		System.out.println("O Titulo da página é;"+titulo);
+	}
+	@Test
+	public void deveInserirValor() {
+		WebElement buscaText = driver.findElement(By.name("q"));
+		buscaText.sendKeys("Selenium");
+		WebElement submitSearchbtn = driver.findElement(By.name("btnK"));
+		submitSearchbtn.submit();
+	}
+	@Test
+	public void radiobutton() {
+		driver.get("http://TestAutomation.co/webelements");
+		java.util.List<WebElement> browserradios = driver.findElements(By.name("browser"));		
+		//browserradios.get(1).Click();
+		for(WebElement radio:browserradios) 
+		{
+			System.out.println(radio.getAttribute("value"));
+			if (radio.getAttribute("value").equals("safari"))
+			{
+				radio.click();
+			}
+		}
+		
+	}
+	
+	
+
+}
---
+++ projetoQuarentena/src/main/java/br/com/automacao/projetoQuarentena/BaseTest.java
@@ -0,0 +1,26 @@
+package br.com.automacao.projetoQuarentena;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+
+/**
+ * Hello world!
+ *
+ */
+public class BaseTest 
+{
+
+	WebDriver driver;
+
+	public BaseTest() {
+		System.setProperty("webdriver.chrome.drive","C:\\Users\\EMCI7\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		driver.manage().window().maximize();
+	}
+	@AfterClass
+	public void encerra() {
+		//driver.quit();
+	}
+}
